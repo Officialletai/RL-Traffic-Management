f9f9ffd19f2491709aceeb736beb24b5bc18d443
Tai Le <officialletai@gmail.com>
2023-06-26 19:07:23 +0100

    Refactored environment to be a little more clear

--- environment.py
+++ environment.py
@@ -5,66 +5,79 @@ import matplotlib.pyplot as plt
 from edge import Edge
 
 class Graph:
+    """
+    Graph is a class representing an undirected, weighted graph.
+    """
+
     def __init__(self, num_nodes=10, sparsity_dist=[0.5, 0.5], max_weight=5, weight_dist=[0.5, 0.2, 0.1, 0.1, 0.1]):
+        """
+        Initialize a graph.
+
+        num_nodes: Number of nodes in the graph.
+        sparsity_dist: Probability mass function for whether an edge exists or not.
+        max_weight: Maximum edge weight.
+        weight_dist: Probability mass function for edge weights.
+        """
         self.num_nodes = num_nodes
         self.sparsity_dist = sparsity_dist
         self.max_weight = max_weight
         self.weight_dist = weight_dist
         self.adjacency = self.generate()
-        
-    
-    def generate(self):
-        # Initialise an n x n adjacency matrix
-        a = np.empty((self.num_nodes, self.num_nodes), dtype=object)
 
-        # Fill in matrix ensuring diaginals are 0 (no self-loops), and that the matrix is symmetric
+    def generate(self):
+        """
+        Generate the adjacency matrix of the graph.
+        """
+        adjacency_matrix = np.empty((self.num_nodes, self.num_nodes), dtype=object)
+
+        # Fill adjacency_matrix with weights
         for i in range(self.num_nodes):
             for j in range(i, self.num_nodes):
-                # Ensure diagonals are zero (no self-loops)
-                if i==j:
-                    a[i][j] = 0
-                # Randomly assign 0 to cell for no connecting edge, or an Edge item with a randomly chosen weight up to a value of self.max_weight (according to the PMF given by self.weight_dist)
+                if i == j:
+                    adjacency_matrix[i][j] = 0
                 else:
-                    a[i][j] = random.choices([0, Edge(random.choices(np.arange(1,self.max_weight+1), weights=self.weight_dist)[0])], weights=self.sparsity_dist)[0]
-                    a[j][i] = a[i][j]
-            
-        return a
+                    weight = random.choices(np.arange(1,self.max_weight+1), weights=self.weight_dist)[0]
+                    edge_or_not = random.choices([0, Edge(weight)], weights=self.sparsity_dist)[0]
+                    adjacency_matrix[i][j] = edge_or_not
+                    adjacency_matrix[j][i] = edge_or_not
+
+        return adjacency_matrix
 
     def print_adjacency(self):
-        # Print the adjacency matrix with weights in place of Edge objects
-        print_adjacency = np.empty((self.num_nodes, self.num_nodes))
+        """
+        Print the adjacency matrix with weights.
+        """
+        print_matrix = np.empty((self.num_nodes, self.num_nodes))
         for i in range(len(self.adjacency)):
             for j in range(len(self.adjacency)):
                 if isinstance(self.adjacency[i][j], Edge): 
-                    print_adjacency[i][j] = self.adjacency[i][j].weight
+                    print_matrix[i][j] = self.adjacency[i][j].weight
                 else:
-                    print_adjacency[i][j] = self.adjacency[i][j]
+                    print_matrix[i][j] = self.adjacency[i][j]
 
-        print(print_adjacency) 
+        print(print_matrix) 
 
     def draw(self):
-        # Create a NetworkX graph object
+        """
+        Draw the graph using matplotlib.
+        """
         G = nx.Graph()
 
-        # Add edges to the graph based on the adjacency matrix
         for i in range(len(self.adjacency)):
             for j in range(i+1, len(self.adjacency[i])):
                 if self.adjacency[i][j] != 0:
                     G.add_edge(i, j)
 
-        # Draw the graph
-        pos = nx.spring_layout(G)  # Specify the layout algorithm for positioning the nodes
+        pos = nx.spring_layout(G)  
         nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, edge_color='gray')
         plt.title("Graph Visualization")
         plt.show()
 
 
-# Create a graph instance
-my_graph = Graph(10)
-# Print attributes and draw graph object
-print(my_graph.num_nodes)
-print(my_graph.adjacency)
-my_graph.draw()
-print(np.all(my_graph.adjacency == my_graph.adjacency.T))
-print(my_graph.print_adjacency())
-
+# Test the graph
+graph = Graph(10)
+print(graph.num_nodes)
+print(graph.adjacency)
+graph.draw()
+print(np.all(graph.adjacency == graph.adjacency.T))
+graph.print_adjacency()
