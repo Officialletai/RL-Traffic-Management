869ed533cf9edcb4e5af14d09c35242724fcf29a
Tai Le <officialletai@gmail.com>
2023-06-28 13:49:23 +0100

    Added speed, distance, costs properties to Edge class

--- edge.py
+++ edge.py
@@ -1,3 +1,7 @@
 class Edge:
-    def __init__(self, weight):
-        self.weight = weight
+    def __init__(self, speed_limit, distance, additional_costs):
+        self.weight = (distance / speed_limit) + additional_costs
+        self.speed_limit = speed_limit
+        self.distance = distance
+        self.additional_costs = additional_costs
+
