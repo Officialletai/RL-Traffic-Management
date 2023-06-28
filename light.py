e1a8e9a09a7e77589d30401d79df7d98c1cc4d6e
Tai Le <officialletai@gmail.com>
2023-06-28 13:58:34 +0100

    Light class, last changed property added + set, if changable function added

--- light.py
+++ light.py
@@ -3,12 +3,25 @@
 class Light:
     def __init__(self):
         self.state = 0
+        self.last_changed = 0
 
-    def change_state(self):
+    def change_state(self, current_time):
+        # The logic can also be written in one-line (but less interpretable):
+        # self.state = int(self.state==0)
         if self.state == 0:
             self.state = 1
         else:
             self.state = 0
         
-        # The logic can also be written in one-line (but less interpretable):
-        # self.state = int(self.state==0)
+        self.last_changed = current_time
+    
+    def if_changable(self, current_time):
+        # If we want to hardcode to enforce periods when lights are unchangable 
+        # ive denoted it as 4 time periods as an example
+        if current_time > self.last_changed + 4:
+            return True
+        return False 
+    
+
+        
+
