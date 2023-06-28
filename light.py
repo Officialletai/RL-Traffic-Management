bb82fc346a51893d6f3821f1fff2a2783078f736
Tai Le <officialletai@gmail.com>
2023-06-28 19:46:27 +0100

    last_changed, current_time properties added to light class

--- light.py
+++ light.py
@@ -1,11 +1,12 @@
 # State space for individual traffic light = R/G, or in binary 0/1
 
 class Light:
-    def __init__(self):
+    def __init__(self, current_time):
         self.state = 0
         self.last_changed = 0
+        self.current_time = current_time
 
-    def change_state(self, current_time):
+    def change_state(self):
         # The logic can also be written in one-line (but less interpretable):
         # self.state = int(self.state==0)
         if self.state == 0:
@@ -13,12 +14,12 @@ class Light:
         else:
             self.state = 0
         
-        self.last_changed = current_time
+        self.last_changed = self.current_time
     
-    def if_changable(self, current_time):
+    def if_changable(self):
         # If we want to hardcode to enforce periods when lights are unchangable 
         # ive denoted it as 4 time periods as an example
-        if current_time > self.last_changed + 4:
+        if self.current_time > self.last_changed + 4:
             return True
         return False 
     
