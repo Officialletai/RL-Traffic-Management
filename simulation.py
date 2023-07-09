aed1611e163e1865cbf82c93a5bb770925de1321
samiulb25 <samiulbasar25@gmail.com>
2023-07-09 19:48:56 +0100

    Created simulation file

---
+++ simulation.py
@@ -0,0 +1,20 @@
+from environment import Graph
+
+"""
+Run a simulation (without agent for now):
+
+1. Initialise Environment
+2. Initialise Cars
+3. Add Cars to queues at Nodes
+
+for t in range(T) (or until completion of episode i.e. every car at its destination):
+    set lights everywhere (randomly if without agent)
+    move cars from queues by checking the light
+    check cumulative distance left to destination from all cars
+"""
+
+def main():
+    pass
+
+if __name__ =='__main__':
+    main()
