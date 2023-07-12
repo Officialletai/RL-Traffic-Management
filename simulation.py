59b449915365f426978ce976727574e155dc1d19
Tai Le <officialletai@gmail.com>
2023-07-12 23:15:43 +0100

    gitignore added, simulation.py + initialise cars

--- simulation.py
+++ simulation.py
@@ -1,10 +1,15 @@
 from environment import Graph
+from car import Car
+
+import numpy as np
+import random
+
 
 """
 Run a simulation (without agent for now):
 
 1. Initialise Environment
-2. Initialise Cars
+2. Initialise Cars 
 3. Add Cars to queues at Nodes
 
 for t in range(T) (or until completion of episode i.e. every car at its destination):
@@ -12,9 +17,28 @@ for t in range(T) (or until completion of episode i.e. every car at its destinat
     move cars from queues by checking the light
     check cumulative distance left to destination from all cars
 """
+class Simulation:
+    def __init__(self):
+        self.environment = Graph()
+        self.cars = self.initialise_cars(num_cars=10)
+        self.time = 0
+        self.score = 0
 
-def main():
-    pass
+
+    def initialise_cars(self, num_cars):
+        cars_list = []
+        for index in range(num_cars):
+            start = random.randrange(0, self.environment.num_nodes - 1)
+            stop = random.randrange(0, self.environment.num_nodes - 1)
+            car = Car(index, self.environment, start, stop)
+            cars_list.append(car)
+
+        return cars_list
+
+
+    def main():
+        pass
 
 if __name__ =='__main__':
-    main()
+    test = Simulation()
+    print(test.cars)
