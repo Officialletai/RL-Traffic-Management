c5d20a2ddad74be0726d316711446ba7060be9b3
samiulb25 <samiulbasar25@gmail.com>
2023-07-15 12:11:28 +0100

    Added functionality to update pointers and minor bug fixing for initialising pointers

--- node.py
+++ node.py
@@ -29,7 +29,11 @@ class Node:
         return edge_labels
     
     def get_queues(self):
-
+        """
+        Initialises the necessary amount of queues for an intersection and stores them in a dictionary in the
+        format {'edge coming into node': queue}, where 'edge coming into node' is labelles as A, B, C or D as
+        defined in the get_edge_labels() method. Queues are list structures as of now. 
+        """
         queues = {}
         keys = 'ABCD'
 
@@ -39,24 +43,42 @@ class Node:
         return queues
     
     def get_pointers(self):
+        """
+        Initialises the pointers for each queue at an intersection: for a node of degree n, there are n-1 pointers
+        for each degree/edge, hence n x (n-1) pointers in total for a node. These are stored as a dictionary inside
+        another dictionary in the following format (example):
 
+        self.pointers = {'A': {'B': None, 'C': None}, 'B': {'A': None, 'C': None}, 'C': {'A': None, 'B': None}}
+
+        The first item in the outer dictionary has key 'A' and thus refers to the edge labelled 'A' (as per the
+        get_edge_labels() method). Cars from edge 'A' at this particular node could either want to go to edge 'B'
+        or 'C' (3-way intersection). This is shown in the inner dictionary that belongs to key 'A'. Here we have 
+        keys 'B' and 'C' showing cars could want to go to either of these two edges from 'A'. The values of 'B'
+        and 'C' are the index of the first car wanting to go to one of these edges from the queue belonging to
+        key/edge 'A' in self.queues.
+        """
         pointers = {}
         keys = 'ABCD'
 
         for i in range(self.degree):
             # pointers[keys[i]] = [None for next_edge in range(self.degree - 1)]
-            pointers[keys[i]] = {next_edge: None for next_edge in keys if next_edge != keys[i]}
+            pointers[keys[i]] = {next_edge: None for next_edge in keys[:self.degree] if next_edge != keys[i]}
         return pointers
 
     def update_pointers(self):
-        
-        
+        """
+        Updates the pointers to ensure the correct car is tracked. Needs to be run every time a change occurs in
+        the queues (e.g. car joins or leaves the queue) as the first index of a car wanting to go to a certain edge
+        could change whenever a car leaves or joins the queue. 'None' type is used for when there are no cars in a
+        particular queue wanting to go to an edge. 
+        """
         for edge in self.pointers:
             edge_destinations = [car.next for car in self.queues[edge]]
             for pointer in self.pointers[edge]:
-                # print(pointer)
-                # 
-                pass
+                try:
+                    self.pointers[edge][pointer] = edge_destinations.index(pointer)
+                except:
+                    self.pointers[edge][pointer] = None
 
 # it has degree queues
 # it has degree x (degree-1) traffic lights for fully defined case
