817794380b8c9dd293e935bd5f24328d5e85e39e
Tai Le <officialletai@gmail.com>
2023-08-07 21:24:10 +0100

    fixed reward

--- environment.py
+++ environment.py
@@ -204,8 +204,7 @@ class Environment:
         for car in self.cars[:]:
             if car.finished:
                 print(f'car {car.id} has finished')
-                reward = car.calculate_reward()
-                self.score += reward
+                self.score += car.reward
                 self.cars.remove(car)
                 continue
             else:
