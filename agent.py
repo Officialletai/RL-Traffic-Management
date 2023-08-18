3837f4eb9c534b2c8f558707f5099e09eb526d5d
Tai Le <officialletai@gmail.com>
2023-08-18 12:53:01 +0100

    work in progress commented out validation method

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 #Reinforcement Learning, Multiagent, IQL DQN
+from collections import deque
 import random
 from environment import Environment
 import torch
@@ -138,12 +139,22 @@ class MultiAgent:
         self.epsilon = max(self.epsilon * self.epsilon_decay_rate, self.epsilon_min)
 
 
-def train_multi_agent(episodes=1250, epsilon_decay_rate=0.995, discount_factor=0.995, learning_rate=0.0005):
-    env = Environment()
+def train_multi_agent(num_nodes=10, sparsity_dist=[0.35, 0.65], num_cars=10, episodes=1250, epsilon_decay_rate=0.995, discount_factor=0.995, learning_rate=0.0005, trial=None, patience=3, validation_episodes=5):
+
+    env = Environment(num_nodes, sparsity_dist, num_cars)
     env.map.draw()
     multi_agent = MultiAgent(env, epsilon_decay_rate, discount_factor, learning_rate)
     times = []
     average_car_wait_time = []
+    recent_avg_wait_times = deque(maxlen=5)
+    
+    # for validation, we swap the sparsity distribution around for variance
+    validation_env = Environment(num_nodes=num_nodes, sparsity_dist=[sparsity_dist[1] , sparsity_dist[0]], num_cars=num_cars)
+    validation_env.map.draw()
+
+    validation_performance = []
+    best_validation_avg_wait_time = float('inf')
+    no_improvement_counter = 0
     
     for episode in range(episodes):
         env.reset()
@@ -168,19 +179,66 @@ def train_multi_agent(episodes=1250, epsilon_decay_rate=0.995, discount_factor=0
 
         avg_time = sum(env.time_in_traffic.values()) / len(env.time_in_traffic)
         average_car_wait_time.append(avg_time)
+        recent_avg_wait_times.append(np.mean(average_car_wait_time))
+        smoothed_avg_wait_time = np.mean(recent_avg_wait_times)
 
         if episode % 25 == 0 and episode != 0:
             print(
                 f"Epochs: {episode}, average_car_wait_time: {np.mean(average_car_wait_time)}, lowest_car_wait_time: {np.amin(average_car_wait_time)}, epsilon_value: {multi_agent.epsilon}"
             ) 
 
+            if trial:
+                trial.report(-smoothed_avg_wait_time, episode)
+                if episode > 100:
+                    if trial.should_prune():
+                        raise optuna.TrialPruned()
+
             average_car_wait_time = []
             times = []
             multi_agent.save_models("dqn_models")
+
+            # Now to validate the model 
+            # total_validation_wait_time = 0
+            # num_validation_episodes = validation_episodes
+
+            # for _ in range(num_validation_episodes):
+            #     validation_env.reset()
+            #     local_states = validation_env.get_local_state()
+            #     finished = False 
+
+            #     while not finished:
+            #         actions = multi_agent.get_actions(local_states)
+            #         actions_array = list(actions.items())
+            #         next_local_states, rewards, finished = validation_env.step(actions_array)
+            #         # Skip training step
+            #         local_states = next_local_states
+                
+            #     average_validation_wait_time = total_validation_wait_time / num_validation_episodes
+            #     validation_performance.append(average_validation_wait_time)
+
+            #     if average_validation_wait_time < best_validation_avg_wait_time:
+            #         best_validation_avg_wait_time = average_validation_wait_time
+            #         no_improvement_counter = 0
+            #     else:
+            #         no_improvement_counter += 1
+
+            #     # Early stopping based on validation performance
+            #     if no_improvement_counter >= patience:
+            #         print(f"Early stopping at episode {episode} due to no improvement in validation performance.")
+            #         break
     
     return times
 
 if __name__ == "__main__":
+    NUM_NODES = 10
+    # Try not to have 50/50 sparsity so that our validation env can have some variance
+    # Might be a better to handle validation env vs normal env
+    SPARSITY_DIST=[0.35, 0.65]
+    NUM_CARS=10
+    EPISODES=2500
+    N_TRIALS=100
+    PATIENCE=3
+    VALIDATION_EPISODES=5
 
     def objective_function(trial):
         discount_factor = trial.suggest_float("discount_factor", 0.995, 0.9999, step=0.0001)
@@ -188,12 +246,22 @@ if __name__ == "__main__":
         epsilon_decay_rate = trial.suggest_float("epsilon_decay_rate", 0.999, 0.9999, step=0.0001)
 
         print(f"Training with discount_factor={discount_factor}, learning_rate={learning_rate}, epsilon_decay_rate={epsilon_decay_rate}")
-        performance = train_multi_agent(2500, epsilon_decay_rate, discount_factor, learning_rate)
+        performance = train_multi_agent(
+            num_nodes=NUM_NODES, 
+            sparsity_dist=SPARSITY_DIST, 
+            num_cars=NUM_CARS, 
+            episodes=EPISODES, 
+            epsilon_decay_rate=epsilon_decay_rate, 
+            discount_factor=discount_factor, 
+            learning_rate=learning_rate,
+            patience=PATIENCE,
+            validation_episodes=VALIDATION_EPISODES
+            )
         
         return performance
 
     test = optuna.create_study(direction="maximize")
-    test.optimize(objective_function, n_trials=100)
+    test.optimize(objective_function, n_trials=N_TRIALS)
 
     best_params = test.best_params
     best_reward = test.best_value
